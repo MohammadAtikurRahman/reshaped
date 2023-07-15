218ce13681df56e4752106decf50144f73992d73
Atikur Rahman <46051298+MohammadAtikurRahman@users.noreply.github.com>
2023-07-15 13:37:54 +0600

    vice versa eiin to school name issue

--- src/sa.js
+++ src/sa.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import {
   Button,
   TextField,
@@ -7,10 +6,10 @@ import {
   DialogTitle,
   DialogContent,
 } from "@material-ui/core";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import Popper from "@material-ui/core/Popper";
+import { useState } from "react";
 import swal from "sweetalert";
 import Swal from "sweetalert2";
+import { useEffect } from 'react';
 
 const axios = require("axios");
 const baseUrl = process.env.REACT_APP_URL;
@@ -20,36 +19,21 @@ export function AddBeneficiary(props) {
   const [beneficiary, setBeneficiary] = useState({});
   const [error, setError] = useState(false);
 
-  const [eiinInput, setEiinInput] = useState("");
-  const [nameInput, setNameInput] = useState("");
-
   const [submit, setSubmit] = useState(false);
-
-  // Schools data
-  const schools = [
-    { eiin: "111794", name: "Sakhuya Adarsha Biddyaniketan" },
-    { eiin: "130875", name: "Shaheed Muktijoddha Girls High School" },
-    { eiin: "107827", name: "Ruhul Amin Khan Uccho Biddaloy" },
-    { eiin: "104414", name: "Fateyabad Mohakali Balika Uccho Biddaloy" },
-    { eiin: "106916", name: "Shyamganj Uccho Biddaloy" },
-    { eiin: "138307", name: "Biam Laboratory School" },
-    { eiin: "117769", name: "The Old Kushtia HighSchool" },
-    { eiin: "131212", name: "Chulash Adarsha Uccho Biddaloy" },
-  ];
 
   useEffect(() => {
     if (submit && beneficiary.u_nm && beneficiary.beneficiaryId) {
       makeRequest();
     }
   }, [submit, beneficiary]);
-
+  
   async function makeRequest() {
     try {
       const res = await axios.post(baseUrl + "/beneficiary/", {
         beneficiary: beneficiary,
         token: localStorage.getItem("token"),
       });
-
+  
       if (res.status === 200) {
         handleEditModalClose();
         Swal.fire({
@@ -78,30 +62,33 @@ export function AddBeneficiary(props) {
     }
     setSubmit(false);
   }
-
+  
   async function addBeneficiary(e) {
     e.preventDefault();
-
+  
     // Appending 'L' to u_nm and 'P' to f_nm before appending the beneficiaryId (eiin)
     setBeneficiary((prevState) => ({
       ...prevState,
       u_nm: `${prevState.beneficiaryId}-L${prevState.u_nm}`,
       f_nm: `${prevState.beneficiaryId}-P${prevState.f_nm}`,
     }));
-
+  
     setSubmit(true);
   }
+  
+
 
   function update(event) {
     let { name, value } = event.target;
+
     if (value === null) {
       value = "";
     }
+
     setBeneficiary((beneficiary) => {
       return { ...beneficiary, [name]: value };
     });
   }
-
   function checkNumber(e) {
     if (isNaN(e.target.value)) {
       swal("Oops!", "Please enter a number", "error");
@@ -115,20 +102,18 @@ export function AddBeneficiary(props) {
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
       maxWidth="xs"
-      // scroll="false" // This will disable the scroll
-
-      style={{ zIndex: 1300 }}
+      style={{ zIndex: "9999"}}
     >
-      <DialogContent style={{ padding: "80px" }}>
+      <DialogContent style={{ padding: "40px" }}>
         <DialogTitle id="alert-dialog-title">
           <span style={{ color: "#138D75" }}>
             {" "}
             <b> Add School </b>{" "}
           </span>
         </DialogTitle>
+ 
         <br />
-
-        {/* <TextField
+        <TextField
           id="standard-basic"
           type="text"
           autoComplete="off"
@@ -140,91 +125,21 @@ export function AddBeneficiary(props) {
           required
           pattern="[0-9]*"
           fullWidth
-        /> */}
-
-        <Autocomplete
-          id="eiin-combo-box"
-          options={schools}
-          getOptionLabel={(option) => option.eiin}
-          inputValue={eiinInput}
-          onInputChange={(event, newInputValue) => {
-            setEiinInput(newInputValue);
-            const selectedSchool = schools.find(
-              (school) => school.eiin === newInputValue
-            );
-            if (selectedSchool) {
-              setBeneficiary((prevState) => ({
-                ...prevState,
-                beneficiaryId: selectedSchool.eiin,
-                name: selectedSchool.name,
-              }));
-              setNameInput(selectedSchool.name);
-            }
-          }}
-          filterOptions={(options, params) => {
-            if (params.inputValue === "") {
-              return [];
-            }
-            const filtered = options.filter((option) =>
-              option.eiin.includes(params.inputValue)
-            );
-            return filtered;
-          }}
-          renderInput={(params) => (
-            <TextField {...params} label="School EIIN" variant="outlined" />
-          )}
-          PopperComponent={({ children, ...props }) => (
-            <Popper {...props} style={{ zIndex: 2000 }}>
-              {children}
-            </Popper>
-          )}
-        />
-
-        <br/>
-        <br />
-
-        <Autocomplete
-          id="name-combo-box"
-          options={schools}
-          getOptionLabel={(option) => option.name}
-          inputValue={nameInput}
-          onInputChange={(event, newInputValue) => {
-            setNameInput(newInputValue);
-            const selectedSchool = schools.find(
-              (school) =>
-                school.name.toLowerCase() === newInputValue.toLowerCase()
-            );
-            if (selectedSchool) {
-              setBeneficiary((prevState) => ({
-                ...prevState,
-                beneficiaryId: selectedSchool.eiin,
-                name: selectedSchool.name,
-              }));
-              setEiinInput(selectedSchool.eiin);
-            }
-          }}
-          filterOptions={(options, params) => {
-            if (params.inputValue === "") {
-              return [];
-            }
-            const filtered = options.filter((option) =>
-              option.name
-                .toLowerCase()
-                .includes(params.inputValue.toLowerCase())
-            );
-            return filtered;
-          }}
-          renderInput={(params) => (
-            <TextField {...params} label="School Name" variant="outlined" />
-          )}
-          PopperComponent={({ children, ...props }) => (
-            <Popper {...props} style={{ zIndex: 2000 }}>
-              {children}
-            </Popper>
-          )}
         />
         <br />
         <br />
+        <TextField
+          id="standard-basic"
+          type="text"
+          autoComplete="off"
+          name="name"
+          value={beneficiary.name}
+          onChange={update}
+          placeholder="School Name"
+          required
+          fullWidth
+        />
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
         <TextField
           id="standard-basic"
           type="text"
@@ -235,8 +150,7 @@ export function AddBeneficiary(props) {
           placeholder="Lab Id "
           fullWidth
         />
-        <br />
-        <br />
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
         <TextField
           id="standard-basic"
           type="text"
@@ -247,9 +161,10 @@ export function AddBeneficiary(props) {
           placeholder="PC ID"
           fullWidth
         />
-        <br />
-        <br />
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      
       </DialogContent>
+
       <DialogActions style={{ paddingRight: "80px", paddingBottom: "50px" }}>
         <Button
           onClick={handleEditModalClose}
